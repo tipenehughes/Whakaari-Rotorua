1b1df19ad4ff70680c58ae493e759891ccb26b33
Tipene Hughes <tipenehughes@gmail.com>
2020-09-22 15:44:48 -0400

    Added form validation and netlify submissions

--- main.js
+++ main.js
@@ -216,6 +216,65 @@ langList.forEach((item) => {
     });
 });
 
+// Form Validation
+
+const form = document.getElementById("form");
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const message = document.getElementById("message");
+
+const submit = document.getElementById("submit");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    checkInputs();
+});
+
+function checkInputs() {
+    // get values from inputs
+    const nameValue = name.value.trim();
+    const emailValue = email.value.trim();
+    const messageValue = message.value.trim();
+
+    if (nameValue === "") {
+        setErrorFor(name, "Name cannot be blank");
+    } else {
+        setSuccessFor(name);
+    }
+    if (emailValue === "") {
+        setErrorFor(email, "Email cannot be blank");
+    } else if(!isEmail(emailValue)) {
+        setErrorFor(email, "Email is not valid");
+    } else {
+        setSuccessFor(email);
+    }
+    if (messageValue === "") {
+        setErrorFor(message, "Message cannot be blank");
+    } else {
+        setSuccessFor(message);
+    }
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector("small");
+
+    small.innerText = message;
+
+    formControl.className = "form-control error";
+}
+
+function setSuccessFor(input) {
+    const formControl = input.parentElement;
+    formControl.className = "form-control success";
+}
+
+function isEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        email
+    );
+}
+
 // Hamburger menu
 
 const mobileMenu = document.getElementById("mobileMenu");
@@ -249,7 +308,7 @@ var flkty = new Flickity(elem, {
     // options
     cellAlign: "center",
     contain: true,
-    imagesLoaded: true
+    imagesLoaded: true,
 });
 
 // element argument can be a selector string
